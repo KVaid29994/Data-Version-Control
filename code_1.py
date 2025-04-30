8d526cd3a2cc350e5dbe776ea44cf7515126cb1c
Kashish <kashish.vaid29994@gmail.com>
2025-04-30 18:59:21 +0530

    Saving the V2 of our data

--- code_1.py
+++ code_1.py
@@ -11,7 +11,9 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
-
+##Adding new row to df for V2
+new_row_loc = {'Name': 'V2', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 
 
@@ -28,3 +30,4 @@ file_path = os.path.join(data_dir, 'sample_data.csv')
 df.to_csv(file_path, index=False)
 
 print(f"CSV file saved to {file_path}")
+
